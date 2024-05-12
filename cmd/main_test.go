c2a70ccb2644f3a4d0cd1727369943f719d67b68
Rafael Fernando <rafaelribeirosouza86@gmail.com>
2024-05-12 20:22:53 -0300

    adding test

---
+++ cmd/main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"strconv"
+	"sync"
+	"testing"
+
+	"github.com/rafaelsouzaribeiro/producer/pkg"
+	exec "github.com/rafaelsouzaribeiro/producer/pkg/kafka"
+)
+
+var count int = 0
+var producer = exec.NewBrokers([]string{"springboot:9092"})
+var wg sync.WaitGroup
+
+func BenchmarkWriter(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		count++
+
+		ms := pkg.Message{
+			Value: "Test" + strconv.Itoa(count),
+			Topic: "contact-adm-insert",
+			Headers: []pkg.Header{
+				{
+					Key:   "your-header-key" + strconv.Itoa(count),
+					Value: "your-header-value" + strconv.Itoa(count),
+				},
+				{
+					Key:   "your-header-key" + strconv.Itoa(count),
+					Value: "your-header-value" + strconv.Itoa(count),
+				},
+			},
+		}
+
+		wg.Add(1)
+
+		go func() {
+			producer.Send(&ms)
+			wg.Done()
+		}()
+		wg.Wait()
+	}
+
+}
